d828ebbce85886984dbf0b894fff2dbfd8e4f913
Magnus von Koeller <hello@magnusvk.com>
2014-09-18 17:56:38 -0400

    Move loading the configuration back into the initializer
    Otherwise, it's not ready in time for some of our tests. We call it from
    to_prepare, also, that way we get the best of both worlds.

--- lib/spree_config_preferences/engine.rb
+++ lib/spree_config_preferences/engine.rb
@@ -9,11 +9,17 @@ module SpreeConfigPreferences
       g.test_framework :rspec
     end
 
+    initializer "spree.config_preferences", :before => "spree.environment" do |app|
+      self.class.load_configurations
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+    end
 
+    def self.load_configurations
       if Rails.configuration.respond_to? :spree_config_preferences_preference_files
         Rails.configuration.spree_config_preferences_preference_files.each do |pref_file|
           Spree::ConfigPreferenceLoader.load(pref_file)
@@ -33,6 +39,11 @@ module SpreeConfigPreferences
       end
     end
 
-    config.to_prepare &method(:activate).to_proc
+    def self.to_prepare
+      activate
+      load_configurations
+    end
+
+    config.to_prepare &method(:to_prepare).to_proc
   end
 end
